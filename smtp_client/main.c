02112dc38df1a6e2e9b436ab248d890929e020bf
ilya27ilya <mam@macbook-pro-777.local>
2018-10-07 12:21:57 +0300

    Initial Commit

---
+++ smtp_client/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  smtp_client
+//
+//  Created by mam on 07.10.2018.
+//  Copyright © 2018 mam. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
